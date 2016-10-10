25b895376a06cf4c989235758efc7cb26e2883a1
j-m-c-p <jhonatancubi@gmail.com>
2016-10-10 12:33:24 -0500

    Create librerias.php
    Librerias.php  le da el estilo

---
+++ librerias.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/miestilo.css">
+
+<script type="text/javascript" src="js/jssor.slider.min.js"></script>
+<script src="js/jquerymin.js"></script>
+<script src="js/bootstrap.min.js"></script>
